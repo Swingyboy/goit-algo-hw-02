57ab36618af958b493c7946380f2729958bdf3d9
serhii <sbezuhlyi@tektelic.com>
2024-04-28 08:12:00 +0300

    make is_palindrome() case-independent

--- task_2.py
+++ task_2.py
@@ -3,6 +3,8 @@ from collections import deque
 
 def is_palindrome(string: str) -> bool:
     queue = deque()
+    string: str = string.lower().replace(" ", "")
+
     for ch in string:
         queue.append(ch)
 
@@ -10,3 +12,9 @@ def is_palindrome(string: str) -> bool:
         if queue.popleft() != queue.pop():
             return False
     return True
+
+
+if __name__ == "__main__":
+    print(is_palindrome("Able was I ere I saw Elba"))
+    print(is_palindrome("Hello"))
+    print(is_palindrome("Was it a car or a cat I saw"))
